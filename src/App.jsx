e7ee000c970a647409e66b4849fa8ffe6dbe3094
CAMILO <cestrad5@gmail.com>
2023-08-11 18:55:32 -0500

    Solving marge

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+function App() {
+
+const [presupuesto, setPresupuesto] = useState('')
+const [isValidPresupuesto, setIsValidPresupuesto]= useState(false)
+
+  return (
+    <div>
+        <Header 
+            presupuesto={presupuesto}
+            setPresupuesto={setPresupuesto}
+            isValidPresupuesto={isValidPresupuesto}
+            setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+    </div>
+  )
+}
+
+export default App
