edb14af6965df2a1fd2411655f64512b014ef442
CAMILO <cestrad5@gmail.com>
2023-08-13 10:53:31 -0500

    Adding a modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import Header from './components/Header'
-
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 function App() {
 
 const [presupuesto, setPresupuesto] = useState('')
 const [isValidPresupuesto, setIsValidPresupuesto]= useState(false)
+const [modal, setModal] = useState(false)
 
+
+const handleNuevoGasto = () => {
+    setModal(true)
+
+
+
+}
   return (
     <div>
         <Header 
@@ -14,6 +23,23 @@ const [isValidPresupuesto, setIsValidPresupuesto]= useState(false)
             isValidPresupuesto={isValidPresupuesto}
             setIsValidPresupuesto={setIsValidPresupuesto}
         />
+
+        {isValidPresupuesto && (
+            <div className='nuevo-gasto'>
+                <img 
+                    src= {IconoNuevoGasto}
+                    alt='icono nuevo gasto'
+                    onClick={handleNuevoGasto}
+                
+                />
+        </div>
+        )}
+
+        {modal && <Modal 
+                    setModal={setModal}
+        
+        
+        />}
     </div>
   )
 }
